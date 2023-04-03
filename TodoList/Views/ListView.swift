cb200d6b366aa9da3182567f1ee8ed7fdbdeedd7
Jack Dellamea <jdellamea@lcs.on.ca>
2023-04-03 15:06:12 -0400

    Setup complete

---
+++ TodoList/Views/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  TodoList
+//
+//  Created by Jack Dellamea on 4/3/23.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
