0f1da66086f4836d6a441f5c31fe8019e78f1fcf
Jack Dellamea <jdellamea@lcs.on.ca>
2023-04-03 14:47:52 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Jack Dellamea on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
